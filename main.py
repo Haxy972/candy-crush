e751b934211216ba21998a6614638d429c65f737
Léo <Haxy@outlook.fr>
2026-04-24 08:48:42 +0200

    🚚 Move package

---
+++ main.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+from random import randint choice
+
+def init_grille(taille):
+  grille = []
+  return grille
+  
+def init_jeu(grille):
+  return 
+
+def detecte_coordonnees_combinaison (griLle, i, j):
+  """
+  Renvoie une liste contenant les coordonnées de tous les bonbons
+  appartenant à la combinaison du bonbon ( i , j ) .
+  """
+
+def affiche_grille(jeu):
+  for i in range(len(jeu)):
+      print()
+      for j in range(len(jeu[i])):
+          print(jeu[i][j],end=" ")
---
+++ utils/list_util.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+
+def copie_l(liste):
+    liste_c = []
+    for i in range(len(liste)):
+        ligne = []
+        for j in range(len(liste)):
+            ligne.append(liste[i][j])
+        liste_c.append(ligne)
+    return liste_c
