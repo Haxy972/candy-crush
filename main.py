cfe123ea1328cc253ab643885a1d0ec279726a64
D_iiegoo <diego.aparicio41350@gmail.com>
2026-04-24 09:47:28 +0200

    Fonction saisie coordoonées
    ajout de la fonction saisie coordoonées

--- main.py
+++ main.py
@@ -19,3 +19,29 @@ def affiche_grille(jeu):
       print()
       for j in range(len(jeu[i])):
           print(jeu[i][j],end=" ")
+
+def saisie_coord(grille):
+    """ 
+    Permet de saisir les coordoonées du bonbon que l'on voudra déplacer
+    param : grille (liste 2D)
+    return : coord_x(int), coord_y(int)
+    """
+    test = True 
+    
+    while test == True:
+        try:
+            coord_x = int(input("Saisissez X : "))
+            coord_y = int(input("Saisissez Y : "))
+
+            
+            if 0 <= coord_x < len(grille) and 0 <= coord_y < len(grille[0]):
+                return coord_x, coord_y
+            else:
+                print("Coordonnées hors de la grille.")
+
+        except ValueError:
+            print("Veuillez saisir un nombre.")
+
+
+liste = [[5, 5, 5, 5, 5], [5, 5, 5, 5, 5], [5, 5, 5, 5, 5], [5, 5, 5, 5, 5], [5, 5, 5, 5, 5]]
+print(saisie_coord(liste))
